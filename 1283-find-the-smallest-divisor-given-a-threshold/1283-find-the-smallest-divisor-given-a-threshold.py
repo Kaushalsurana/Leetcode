d0f858c0f7debde75f5d94928ef74e8c901260d3
kaushal Surana <114735073+Kaushalsurana@users.noreply.github.com>
2025-01-14 18:05:44 +0530

    Time: 131 ms (43.6%), Space: 22.5 MB (24.55%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -3,9 +3,9 @@ class Solution:
         low,high=1,max(nums)
         while low<=high:
             mid=(low+high)//2
-            k=0
-            for i in nums:
-                k+=math.ceil(i/mid)
+            k=sum(math.ceil(i/mid) for i in nums)
+            # for i in nums:
+            #     k+=math.ceil(i/mid)
             if k<=threshold:
                 high=mid-1
             else:
