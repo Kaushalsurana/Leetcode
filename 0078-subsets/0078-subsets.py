c2a57ccbb870db150120bb84be3cebc1b12376cd
kaushal Surana <114735073+Kaushalsurana@users.noreply.github.com>
2025-02-13 17:03:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (38.22%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,10 +1,12 @@
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[[]]
-        for i in nums:
-            p=len(res)
-            for j in range(p):
-                s=list(res[j])
-                s.append(i)
-                res.append(s)
+        res=[]
+        def gen(a,arr):
+            res.append(arr[:])
+            for i in range(a,len(nums)):
+                arr.append(nums[i])
+                gen(i+1,arr)
+                arr.pop()
+        gen(0,[])
         return res
