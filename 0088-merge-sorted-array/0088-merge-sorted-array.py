c7a2cda8241e07d69a15fa603f1b260cf638a77a
kaushal Surana <114735073+Kaushalsurana@users.noreply.github.com>
2025-08-18 12:02:53 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        if n==0:
+            return nums1
         for i in range(m,m+n):
             nums1[i]=nums2[i-m]
+        if nums1[m-1]<nums1[m]:
+            return nums1
         return nums1.sort()
